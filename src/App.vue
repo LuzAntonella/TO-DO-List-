f64571cf1e8b9a3ddf142a8bdcd7eda8e26a120f
LuzAntonella <cabrejosseverinoluzantonella@gmail.com>
2022-09-07 08:28:16 -0500

    desarrollo de interfaz

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Lista />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Lista from "./components/Lista.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Lista,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 480px;
+  width: 100%;
+  max-height: 100%;
+  background-color: rgb(255, 255, 255);
+  padding: 25px;
+  border-radius: 25px;
+  /*overflow: auto;*/
+  /*color: #222;*/
 }
 </style>
---
+++ src/components/Lista.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cont">
+    <h1>TO DO LIST</h1>
+    <div class="add-task">
+      <input
+        type="text"
+        placeholder=" Elemento de la lista"
+        class="task-input"
+      />
+      <input type="submit" value="" class="submit-task" title="Add Task" />
+    </div>
+    <ul class="task-list">
+      <li class="task-list-item">
+        <label class="task-list-item-label">
+          <input type="checkbox" />
+          <span>Ir a concierto de jamiroquai</span>
+        </label>
+        <span class="delete-btn" title="Delete Task"></span>
+      </li>
+    </ul>
+    <br />
+    <hr />
+    <ul class="task-complet">
+      <li>
+        <input type="checkbox" name="list" id="nivel1-1" /><label
+          class="desplegable"
+          for="nivel1-1"
+          >Elementos completados</label
+        >
+        <ul class="interior">
+          <li class="task-list-item">
+            <label class="task-list-item-label">
+              <input type="checkbox" />
+              <span>Task</span>
+            </label>
+            <span class="delete-btn" title="Delete Task"></span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Lista",
+  data() {},
+};
+</script>
+<style>
+@import "../assets/css/styles.css";
+</style>
